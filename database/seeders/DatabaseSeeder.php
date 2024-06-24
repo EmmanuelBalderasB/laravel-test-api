e28c29632c74352752ba05c2c640ed67e016e1fe
Emmanuel <ebalderasbeq@gmail.com>
2024-06-24 11:25:12 -0600

    changes factory to customer factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Customer;
+use databases\factories\CustomerFactory;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,10 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            CustomerSeeder::class,
         ]);
     }
 }
