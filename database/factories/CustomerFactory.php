c1009c049e97e320eaf8820153759d01b3ffd78f
Emmanuel <ebalderasbeq@gmail.com>
2024-06-24 10:45:44 -0600

    creates customer and invoice factories

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Customer;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class CustomerFactory extends Factory
+{
+    /**
+     * The current password being used by the factory.
+     */
+    protected $model = Customer::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $type = $this->faker->randomElement(['I', 'B']);
+        $name = $type === 'I' ? $this->faker->name() : $this->faker->company();
+
+        return [
+            'name' => $name,
+            'type' => $type,
+            'email' => $this->faker->unique()->safeEmail(),
+            'address' => $this->faker->streetAddress(),
+            'city' => $this->faker->city(),
+            'state' => $this->faker->stateAbbr(),
+            'zip' => $this->faker->postcode(),
+        ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     */
+    public function unverified(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => null,
+        ]);
+    }
+}
