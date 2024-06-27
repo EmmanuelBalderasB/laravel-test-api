35d49337445b2ecf7409656db7867527280865e1
Emmanuel <ebalderasbeq@gmail.com>
2024-06-26 22:33:39 -0600

    simple query

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,26 @@
-import './bootstrap';
+import "./bootstrap";
+
+async function fetchCustomers() {
+    const tableBody = document.getElementById("tbody");
+    axios.get("http://localhost:8000/api/v1/customers").then((response) => {
+        console.log("data: ", response.data.data);
+        for (const customer of response.data.data) {
+            const tr = document.createElement("tr");
+            tr.innerHTML = `
+                <td>${customer.id}</td>
+                <td>${customer.name}</td>
+                <td>${customer.email}</td>
+                <td>${customer.address}</td>
+                <td>${customer.city}</td>
+                <td>${customer.state}</td>
+                <td>${customer.zipCode}</td>
+                <td>${customer.type === "I" ? "Individual" : "Business"}</td>
+            `;
+            tr.className =
+                "border-black hover:bg-gray-100 px-4 divide-x-2 divide-y-2";
+            tableBody.appendChild(tr);
+        }
+    });
+}
+
+fetchCustomers();
